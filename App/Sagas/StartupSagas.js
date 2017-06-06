0b6bc47d2edb5d02579efd516b11f10d70c6df04
Patrik Prevuznak <patrik.prevuznak@gmail.com>
2017-06-06 13:37:24 +0200

    Remove unnnecessary logs

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -9,28 +9,28 @@ export const selectAvatar = (state) => state.github.avatar
 export function * startup (action) {
   if (__DEV__ && console.tron) {
     // straight-up string logging
-    console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
+    // console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
 
     // logging an object for better clarity
-    console.tron.log({
-      message: 'pass objects for better logging',
-      someGeneratorFunction: selectAvatar
-    })
+    // console.tron.log({
+    //   message: 'pass objects for better logging',
+    //   someGeneratorFunction: selectAvatar
+    // })
 
     // fully customized!
     const subObject = { a: 1, b: [1, 2, 3], c: true }
     subObject.circularDependency = subObject // osnap!
-    console.tron.display({
-      name: '🔥 IGNITE 🔥',
-      preview: 'You should totally expand this',
-      value: {
-        '💃': 'Welcome to the future!',
-        subObject,
-        someInlineFunction: () => true,
-        someGeneratorFunction: startup,
-        someNormalFunction: selectAvatar
-      }
-    })
+    // console.tron.display({
+    //   name: '🔥 IGNITE 🔥',
+    //   preview: 'You should totally expand this',
+    //   value: {
+    //     '💃': 'Welcome to the future!',
+    //     subObject,
+    //     someInlineFunction: () => true,
+    //     someGeneratorFunction: startup,
+    //     someNormalFunction: selectAvatar
+    //   }
+    // })
   }
   // const avatar = yield select(selectAvatar)
   // only get if we don't have it yet
